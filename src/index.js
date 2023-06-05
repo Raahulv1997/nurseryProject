c53c2e25e62703b37a6f44715f9df453e11978a7
Raahulv1997 <117512559+Raahulv1997@users.noreply.github.com>
2023-06-05 20:41:57 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -20,17 +20,7 @@ import "../src/component/css-js/css/wallet.css";
 import "../src/component/css-js/css/invoice.css";
 import "../src/component/css-js/css/checkout.css";
 import CartProvider from "./component/helper/cart/CartProvider";
-// <link rel="stylesheet" to=""  />
 
-//  js
-// import "../src/component/css-js/vendor/bootstrap/jquery-1.12.4.min.js";
-// import "../src/component/css-js/js/main.js";
-// import "../src/component/css-js/vendor/slickslider/slick.min.js";
-// import "../src/component/css-js/js/slick";
-// import "../src/component/css-js/js/venobox.js";
-// import "../src/component/css-js/js/accordion.js";
-// import "../src/component/css-js/js/countdown.js";
-// import "../src/component/css-js/js/nice-select.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -41,7 +31,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
